c776480c0902dc9d8ca0556bf8fcdaf24cd52dfd
femyeda <femiao@users.noreply.github.com>
2020-05-15 18:23:28 -0500

    feat(component): allow render children, custom classes

--- src/templates/component/<%= name %>/<%= name %>.tsx
+++ src/templates/component/<%= name %>/<%= name %>.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import clsx from 'clsx'
 
 type Props = {
-
+  children: any
+  classes?: string
 }
 
 const <%= name%>: React.FunctionComponent<Props> = (props: Props) => {
-  return (
-    <div className='<%= name%>Component'>
 
+  return (
+    <div className={clsx(
+      props?.classes,
+      '<%= name%>Component',
+    )}>
+      {props.children}
     </div>
   )
 }
